c7f6eb38aa730e38828e35d2f23e7eedf6f12c3e
Edvin Gavell <eddi@live.se>
2021-01-12 08:51:13 +0100

    Created project

---
+++ src/Lecture1/Lecture1.java
@@ -0,0 +1,12 @@
+package Lecture1;
+
+import java.util.Base64;
+
+public class Lecture1 {
+    public static void main(String[] args) {
+        //Base64 encoding
+        String originalString = "This is a message";
+        byte[] temp = originalString.getBytes();
+
+    }
+}
