f841bd712abbc72bc6eeef0ef4a8d904f2bc023f
Edvin Gavell <eddi@live.se>
2021-01-12 09:04:07 +0100

    added lecture1

--- src/Lecture1/Lecture1.java
+++ src/Lecture1/Lecture1.java
@@ -6,7 +6,12 @@ public class Lecture1 {
     public static void main(String[] args) {
         //Base64 encoding
         String originalString = "This is a message";
-        byte[] temp = originalString.getBytes();
+        String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
+        System.out.println(encodedString);
 
+        //Base64 decode
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        System.out.println(decodedString);
     }
 }
