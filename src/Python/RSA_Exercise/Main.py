634b9770e0346244e2435be0993df7cf676ab2bc
Edvin Gavell <eddi@live.se>
2021-01-21 15:46:14 +0100

    working rsa_exercise

--- src/Python/RSA_Exercise/Main.py
+++ src/Python/RSA_Exercise/Main.py
@@ -1,15 +1,65 @@
 import RSA
 
 
-def main():
-    # RSA.encrypt_To_File("Shayan", "H")
-    # decrypted_msg = RSA.decrypt_from_file("Shayan", "Shayan_encrypted_msg_2")
-    # print(decrypted_msg)
-    encoded = RSA.encrypt("Shayan", "H")
-    print(encoded)
-    print(RSA.decrypt("Shayan", encoded))
+def generate_keys():
+    key_name = input('Enter key name:')
+    RSA.generate_new_key(key_name)
 
-    RSA.decrypt_from_file("Shayan", "Shayan_encrypted_msg_2")
+
+def encrypt_message_to_string():
+    key_name = input('Enter key: ')
+    message = input('Enter message to encrypt: ')
+    encrypted_msg = RSA.encrypt(key_name, message)
+    print(encrypted_msg)
+
+
+def encrypt_message_to_file():
+    key_name = input('Enter key: ')
+    message = input('Enter message to encrypt: ')
+    RSA.encrypt_To_File(key_name, message)
+
+
+def decrypt_message_from_string():
+    key_name = input('Enter key: ')
+    message = input('Enter message to encrypt: ')
+    decrypted_msg = RSA.decrypt(key_name, message)
+    print(decrypted_msg)
+
+
+def decrypt_message_from_file():
+    key_name = input('Enter key: ')
+    file_name = input('Enter file name to decrypt: ')
+    print(RSA.decrypt_from_file(key_name, file_name))
+
+
+def menu():
+    while True:
+        print('>>>>> ENCRYPTION MENU <<<<<')
+        print('===========================')
+        print('1. Generate keys')
+        print('2. Encrypt: Message -> string')
+        print('3. Encrypt: Message -> file')
+        print('4. Decrypt: Message -> String')
+        print('5. Decrypt: File -> String')
+        print('9. Exit')
+        choice = input('> ')
+
+        if choice == '1':
+            generate_keys()
+        elif choice == '2':
+            encrypt_message_to_string()
+        elif choice == '3':
+            encrypt_message_to_file()
+        elif choice == '4':
+            decrypt_message_from_string()
+        elif choice == '5':
+            decrypt_message_from_file()
+        elif choice == '9':
+            break
+
+
+def main():
+    menu()
 
 
 if __name__ == '__main__':
--- src/Python/RSA_Exercise/RSA.py
+++ src/Python/RSA_Exercise/RSA.py
@@ -1,3 +1,5 @@
+import base64
+import math
 import os
 import sys
 
@@ -12,43 +14,75 @@ class RSA:
         self.name = name
 
 
+def bytes2int(byte_data):
+    return int.from_bytes(byte_data, 'big', signed=False)
+
+
+def int2bytes(number: int, fill_size: int = 0) -> bytes:
+    """
+    Convert an unsigned integer to bytes (big-endian)::
+    Does not preserve leading zeros if you don't specify a fill size.
+    :param number:
+        Integer value
+    :param fill_size:
+        If the optional fill size is given the length of the resulting
+        byte string is expected to be the fill size and will be padded
+        with prefix zero bytes to satisfy that length.
+    :returns:
+        Raw bytes (base-256 representation).
+    :raises:
+        ``OverflowError`` when fill_size is given and the number takes up more
+        bytes than fit into the block. This requires the ``overflow``
+        argument to this function to be set to ``False`` otherwise, no
+        error will be raised.
+    """
+
+    if number < 0:
+        raise ValueError("Number must be an unsigned integer: %d" % number)
+
+    bytes_required = max(1, math.ceil(number.bit_length() / 8))
+
+    if fill_size > 0:
+        return number.to_bytes(fill_size, 'big')
+
+    return number.to_bytes(bytes_required, 'big')
+
+
 def encrypt(key_name, message):
     __readKey(key_name)
-    byte_msg = message.encode()
     _, e, n = RSA.public_key
-    return [pow(c, e, n) for c in byte_msg]
+    byte_msg = message.encode(encoding='utf-8')
+    int_message = bytes2int(byte_msg)
+    return base64.b64encode(int2bytes(pow(int_message, e, n))).decode()
 
 
 def encrypt_To_File(key_name, message):
-    __readKey(key_name)
-    byte_msg = message.encode()
-    key_size, e, n = RSA.public_key
-    encoded_msg = [pow(c, e, n) for c in byte_msg]
+    encoded_msg = encrypt(key_name, message)
     i = 0
-    if os.path.exists(f'Messages/{key_name}_encrypted_msg_' + str(i)):
-        while os.path.exists(f'Messages/{key_name}_encrypted_msg_' + str(i)):
+
+    if os.path.exists(f'Messages/{key_name}_enc_' + str(i)):
+        while os.path.exists(f'Messages/{key_name}_enc_' + str(i)):
             i += 1
-    with open(f'Messages/{key_name}_encrypted_msg_' + str(i), 'w') as f:
+
+    with open(f'Messages/{key_name}_enc_' + str(i), 'w') as f:
         f.write(str(encoded_msg))
-    print("Messages/" + key_name + "_encrypted_msg_" + str(i) + " created in : Messages")
+
+    print("Messages/" + key_name + "_enc_" + str(i) + " created in : Messages")
 
 
 def decrypt(key_name, message):
     __readKey(key_name)
     _, d, n = RSA.private_key
-    return ''.join([chr(pow(c, d, n)) for c in message])
+    message1 = base64.b64decode(message)
+    int_message = bytes2int(message1)
+    result = pow(int_message, d, n)
+    bytes_result = int2bytes(result)
+    return bytes_result.decode('utf-8')
 
 
 def decrypt_from_file(key_name, file_name):
-    __readKey(key_name)
-    _, d, n = RSA.private_key
-    message = open('Messages/' + file_name).readlines()  # Open and read file
-    msg = ''
-    for i in message:
-        msg += i
-    print(msg)
-
-    return ''.join([chr(pow(c, d, n)) for c in msg.encode()])
+    encoded_msg = open('Messages/' + file_name).read()
+    return decrypt(key_name, encoded_msg)
 
 
 def __readKey(key_name):
